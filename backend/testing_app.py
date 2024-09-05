fb32b2c695b2ccc111cdab2b4df12fd291591e5f
willcheu <willcheu47@gmail.com>
2024-09-05 14:59:50 +1000

    moved again

--- backend/testing_app.py
+++ backend/testing_app.py
@@ -1,9 +1,6 @@
 import unittest
-
 from bson.objectid import ObjectId
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.py import app, client, db, users_collection 
 class FlaskAppTests(unittest.TestCase):
     
