09a60c88c2d6b1fa1881300ba7e352b65e16af04
lefi <Blackmoss74@gmail.com>
2024-09-09 16:53:58 +1000

    added functional navbar

--- frontend/src/app/view/navbar.tsx
+++ frontend/src/app/view/navbar.tsx
@@ -1,13 +1,37 @@
-import React from "react";
-import Link from "next/link"
+'use client'
 
-export default function NavBar() {
-    return(
-        <div>
-            <Link href="/calendar">calendar</Link>
-            <Link href="/buildings">buildings</Link>
-            <Link href="/courses">courses</Link>
-            <Link href="/students">students</Link>
-        </div>
-    );
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+export default function Navbar() {
+
+  return (
+    <nav className="mb-4">
+      <ul className="flex space-x-4">
+        <NavLink path='calendar' text='Calendar'></NavLink>
+        <NavLink path='courses' text='Courses'></NavLink>
+        <NavLink path='buildings' text='Buildings'></NavLink>
+        <NavLink path='students' text='Students'></NavLink>
+      </ul>
+    </nav>
+  )
 }
+
+interface LinkProp {
+    path: string
+    text: string
+}
+
+const NavLink: React.FC<LinkProp> = ({ path, text }) =>{
+    return(
+        <li>
+          <Link 
+            href={path} 
+            className={`text-gray-600 hover:text-blue-600`}
+          >
+            {text}
+          </Link>
+        </li>
+    );
+};
