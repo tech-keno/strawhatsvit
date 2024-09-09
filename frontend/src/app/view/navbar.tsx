1453c0c07cd0f816e2d350188ce1356fedd62b17
Eliza Li <zimul2@student.unimelb.edu.au>
2024-09-09 21:33:20 +1000

    navbar visual update

--- frontend/src/app/view/navbar.tsx
+++ frontend/src/app/view/navbar.tsx
@@ -8,11 +8,11 @@ export default function Navbar() {
 
   return (
     <nav className="mb-4">
-      <ul className="flex space-x-4">
-        <NavLink path='calendar' text='Calendar'></NavLink>
-        <NavLink path='courses' text='Courses'></NavLink>
-        <NavLink path='buildings' text='Buildings'></NavLink>
-        <NavLink path='students' text='Students'></NavLink>
+      <ul className="flex space-x-8 bg-orange-500 p-9 ">
+        <li><NavLink path='calendar' text='TIMETABLE'></NavLink></li>
+        <li><NavLink path='buildings' text='BUILDING'></NavLink></li>
+        <li><NavLink path='students' text='PERSONNEL'></NavLink></li>
+        <li><NavLink path='courses' text='COURSES'></NavLink></li>
       </ul>
     </nav>
   )
@@ -28,7 +28,7 @@ const NavLink: React.FC<LinkProp> = ({ path, text }) =>{
         <li>
           <Link 
             href={path} 
-            className={`text-gray-600 hover:text-blue-600`}
+            className={`text-2xl text-gray-600 hover:text-blue-600 font-mono font-extrabold text-white`}
           >
             {text}
           </Link>
