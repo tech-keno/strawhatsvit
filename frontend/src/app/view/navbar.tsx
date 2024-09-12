d8ee596538b13dcd013a786b2ab4ed6d3f2fa749
Ibrahim Bilal <112560006+Ibbssy@users.noreply.github.com>
2024-09-12 16:10:14 +1000

    🧭nav bar logo and logout added

--- frontend/src/app/view/navbar.tsx
+++ frontend/src/app/view/navbar.tsx
@@ -2,17 +2,44 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import axios from 'axios'
 import React from 'react'
+import Image from 'next/image'
+import logo from './vit_logo_alt.png'
 
 export default function Navbar() {
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:5000/logout', { withCredentials: true });
+      if (response.status === 200) {
+        window.location.href = '/login'; // Redirect to login page after logout
+      }
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
 
   return (
     <nav className="mb-4">
-      <ul className="flex space-x-8 bg-orange-500 p-9 ">
-        <li><NavLink path='calendar' text='TIMETABLE'></NavLink></li>
-        <li><NavLink path='buildings' text='BUILDING'></NavLink></li>
-        <li><NavLink path='students' text='PERSONNEL'></NavLink></li>
-        <li><NavLink path='courses' text='COURSES'></NavLink></li>
+      <ul className="flex justify-between items-center bg-orange-500 p-9">
+        <div className="flex space-x-8">
+          <div>
+          <Link href={'calendar'}>
+            <Image src={logo} alt="Logo" width={105} height={105} className="cursor-pointer" />
+          </Link>
+          </div>
+          <div><NavLink path='calendar' text='TIMETABLE'></NavLink></div>
+          <div><NavLink path='buildings' text='BUILDING'></NavLink></div>
+          <div><NavLink path='students' text='PERSONNEL'></NavLink></div>
+          <div><NavLink path='courses' text='COURSES'></NavLink></div>
+        </div>
+        <div>
+          <button
+            onClick={handleLogout} 
+            className="text-2xl text-gray-600 hover:text-blue-600 font-mono font-extrabold text-white">
+            LOGOUT
+          </button>
+        </div>
       </ul>
     </nav>
   )
