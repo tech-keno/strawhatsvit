bacc13b44a8cbdd651fbb90dee83297dffba9a5a
tech-keno <ttechawatcha@student.unimelb.edu.au>
2024-09-25 23:05:08 +1000

    Revert "added export in package.json"
    This reverts commit b7e1e840d79c1fd98f80d24b16c4ccf02dff3527.

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Enable static export
-    // Any other Next.js config options you need
-  };
-  
-  module.exports = nextConfig;
-  
