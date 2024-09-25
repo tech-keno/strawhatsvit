9a160533e4ef7176d04777e6b0dcf4211638508d
tech-keno <ttechawatcha@student.unimelb.edu.au>
2024-09-25 22:56:16 +1000

    testing deploy

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Enable static export
+    // Any other Next.js config options you need
+  };
+  
+  module.exports = nextConfig;
+  
